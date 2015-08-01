1648ed03d0f3ff433a535ab95cb5bcfcfde25b06
gs-mkavali <mkavali@gainsight.com>
2015-08-02 00:36:38 +0530

    passwd changed

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,28 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'prime',
+  password : 'welcome',
   database : 'prime'
 });
 
 connection.connect();
+
+
+
+
+require('plotly')('MurthyKavali', '87s9adlcbj');
+
+var data = [
+  {
+    x: ["giraffes", "orangutans", "monkeys"],
+    y: [20, 14, 23],
+    type: "bar"
+  }
+];
+var graphOptions = {filename: "basic-bar", fileopt: "overwrite"};
+plotly.plot(data, graphOptions, function (err, msg) {
+    console.log(msg);
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -39,7 +56,7 @@ router.get('/persons', function (req, res, next) {
 /*Get Registrations*/
 router.get('/registrations', function (req, res, next) {
   var self= res;
-    connection.query('select first_name, last_name, GROUP_CONCAT(sport_name) as sports from (select reg.person_id, first_name, last_name, sport_name from registrations reg LEFT JOIN persons p ON p.person_id = reg.person_id LEFT JOIN sports s ON s.sport_id = reg.sport_id ) as temp group by temp.person_id;', function(err, rows, fields) {
+    connection.query('select person_id, first_name, last_name, GROUP_CONCAT(sport_name) as sports from (select reg.person_id, first_name, last_name, sport_name from registrations reg LEFT JOIN persons p ON p.person_id = reg.person_id LEFT JOIN sports s ON s.sport_id = reg.sport_id ) as temp group by temp.person_id;', function(err, rows, fields) {
     if (!err){
       // rows = JSON.stringify(rows);
       console.log('The solution is: ', rows);
@@ -61,6 +78,25 @@ router.get('/sport_id/:sport_id', function (req, res, next) {
       // rows = JSON.stringify(rows);
       console.log('The solution is: ', rows);
       self.render('reg-per-sport', {title: 'Registration Per Sport', regData: rows});
+    }else{
+      console.log('Error while performing Query.');
+    }
+
+});
+
+});
+
+/*Get Details per Person*/
+router.get('/person/:person_id', function (req, res, next) {
+  var self= res;
+    var person_id= req.params.person_id;
+    console.log("person id: "+person_id);
+    console.log('select first_name, age, gender, block, flat from persons where person_id='+person_id+';');
+    connection.query('select first_name, age, gender, block, flat from persons where person_id='+person_id+';', function(err, rows, fields) {
+    if (!err){
+      // rows = JSON.stringify(rows);
+      console.log('The solution is: ', rows);
+      self.render('person-details', {title: 'Person Details', regData: rows});
     }else{
       console.log('Error while performing Query.');
     }
